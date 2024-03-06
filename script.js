f406fb22a6ebac5353de6a1ae83e2cc47864337b
EdLuciuz <100480885+EdLuciuz@users.noreply.github.com>
2024-03-06 19:08:14 +0700

    added erase button and change resolution button

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
 const main = document.querySelector('#main')
+const changeButton = document.querySelector('#change')
+const eraseButton = document.querySelector('#erase')
 
-let gridRes = 99;
+let gridRes = 16;
 let gridHeight = 90/gridRes
 
-function createGrids(res) {
+changeButton.addEventListener('click', () => {
+    let gridCon = document.querySelector('.gridCon')
+    main.removeChild(gridCon)
+
+    let gridRes = prompt('Enter the resolution size')
+
+    if (gridRes > 100) {
+        gridRes = 100;
+    }
+
+    let gridHeight = 90/gridRes
+
+    createGrids(gridRes, gridHeight);
+})
+
+eraseButton.addEventListener('click', () => {
+    let gridBox = document.querySelectorAll('.gridBox')
+
+    gridBox.forEach(gridBox => {
+        gridBox.classList.remove('ink')
+    })
+})
+
+function createGrids(res, height) {
     const gridCon = document.createElement('div')
     gridCon.classList.add('gridCon')
     main.appendChild(gridCon)
 
-    for (i = 0; i < res * res; i++) {
+    for (let i = 0; i < res * res; i++) {
         const gridBox = document.createElement('div');
         gridBox.classList.add('gridBox')
-        gridBox.style.width = `${gridHeight}vh`
-        gridBox.style.height = `${gridHeight}vh`
+        gridBox.style.width = `${height}vh`
+        gridBox.style.height = `${height}vh`
+
+        gridBox.addEventListener('mouseover', () => {
+            gridBox.classList.add('ink')
+        })
         gridCon.appendChild(gridBox)
     }
 }
 
-
-createGrids(gridRes);
+createGrids(gridRes, gridHeight);
